714eed358f64fca2343a9872eb0769d08d673b66
Will Redmond <willredmond85@gmail.com>
2021-02-01 15:49:20 -0500

    Completed 2.1

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Will Redmond on 2/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
