b8473dc8ecf1c4b5b4b46fa7aadabe4c05882fc9
Will Redmond <willredmond85@gmail.com>
2021-02-01 16:24:58 -0500

    Push to show message

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("🙏 ViewDidLoad has run!!")
     }
 
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        print("😎 The message button was pressed!")
+        messageLabel.text = "You Are Awesome!"
+    }
+    
 }
 
