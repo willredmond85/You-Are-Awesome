96daf3e480c351d32ca275f29028ac4d6130077d
Will Redmond <willredmond85@gmail.com>
2021-02-08 10:11:17 -0500

    Add image popping up

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("🙏 ViewDidLoad has run!!")
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        print("😎 The message button was pressed!")
         messageLabel.text = "You Are Awesome!"
+        imageView.image = UIImage(named: "image0")
     }
     
 }
